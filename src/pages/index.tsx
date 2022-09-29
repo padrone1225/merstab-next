158e5957d50ea8fb63977c9d7ddf0e1db0fab8e7
padrone1225 <padrone1225@gmail.com>
2022-09-29 20:41:38 +0900

    basicStyle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import type { NextPage } from "next";
+import GradientButton from "../components/GradientButton";
+import MemberCard from "../components/MemberCard";
+import { DisconrdIcon, WalletIcon } from "../components/SvgIcon";
+import cardImage from "../../public/image/c-3.png";
 
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <GradientButton title={"START STAKING"} color />
+      <GradientButton title={"LAUNCH APP"} />
+      <GradientButton title={"CONNECT WALLET"} icon={<WalletIcon />} />
+      <GradientButton title={"JOIN DISCORD"} icon={<DisconrdIcon />} discord />
+      <GradientButton
+        title={"JOIN DISCORD"}
+        icon={<DisconrdIcon />}
+        discord
+        color
+      />
+      <GradientButton title={"CONTACT US"} size={"md"} color />
+      <GradientButton title={"CONTACT US"} size={"sm"} color />
+      <GradientButton title={"CONTACT US"} size={"o"} color />
+      <MemberCard type size={"sm"} image={cardImage} />
+    </>
+  );
 };
 
 export default Home;
